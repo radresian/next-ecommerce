f7396addf48710df9418734ab495e713f3aba467
serdarhamzaogullari <hasanserdarhamza@gmail.com>
2022-04-04 01:30:42 +0300

    landing header

--- components/header/index.js
+++ components/header/index.js
@@ -5,7 +5,6 @@ import HeaderMobile from './header-mobile';
 import HeaderDesktop from './header-desktop';
 import {useCallback} from 'react';
 import Web3 from 'web3';
-import Page from "../page";
 
 export default function Header() {
   const { data, loading, error, refetch } = useQuery(VIEWER);
@@ -40,6 +39,17 @@ export default function Header() {
 
       <nav id="desktop">
         <HeaderDesktop viewer={viewer} connectWallet={connectWallet} />
+        <div className="header-text-container">
+          <h1>Türkiye'de Bir İlk, Türk Sanatçılarının NFT lerini Türk Lirası ile Satın Alın</h1>
+          <h2>eNeF-Turk ile bir crypto cüzdanınız olmasa bile sergilenen sanat eserlerinin NFT haklarını kredi kartınız ile hemen satın alabilirsiniz.</h2>
+          <a
+             href='#content'
+              id='header-text-button'
+                  className='header-text-button'
+          >
+            Hemen NFT lere Göz At
+          </a>
+        </div>
         <div className="wrap">
           <video loop autoPlay muted>
               <source
@@ -59,29 +69,66 @@ export default function Header() {
           background-color: #040277;
           box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.05);
         }
+        h1 {
+          color: white;
+          font-size: 38px;
+          font-weight: 800;
+          margin-bottom: 10px;
+        }
+        h2 {
+          color: white;
+          font-size: 25px;
+          font-weight: 300;
+        }
+       .header-text-button {
+          font-family: 'Raleway', sans-serif;
+          text-transform: uppercase;
+          color: #fff !important;
+          background-color: #694bed;
+          background-image: linear-gradient(to right, #694bed 0%, #3e9ffb 100%);
+          padding: 14px 34px;
+          letter-spacing: 1px;
+          margin-top: 20px;
+          font-size: 15px;
+          font-weight: 500;
+          border-radius: 25px;
+          transition: all 0.5s linear;
+          border: 0;
+          text-decoration: none;
+          display: inline-block;
+        }
+        .header-text-container {
+          position: absolute;
+          top: 200px;
+          left: 100px;
+          z-index: 2;
+        }
         .wrap {
           background: yellow;
-          width: 100%; /* Play with this value */
+          width: 100%;
+          height: 23vw;
           font-size: 0;
-          text-align: center;   
-        }
-        wrap:before {
-          content: "";
-          width: 1px;
-          height: 100%;
-          display: inline-block;
-          vertical-align: middle;
-          margin-left: -1px;
+          text-align: center;
+          z-index: 1;
         }
         .wrap video {
-          max-width: 100%;
-          max-height: 100%;
+          max-width: 155%;
+          max-height: 155%;
           display: inline-block;
           vertical-align: middle;
         }
         #mobile {
           display: none;
           z-index: 0;
+        }
+        @media (max-width: 1256px) {
+          .wrap {
+            height: 100%;
+          }
+          .wrap video {
+            max-width: 100%;
+            max-height: 100%;
+          }
         }
         @media (max-width: 1000px) {
           #mobile {
--- components/page.js
+++ components/page.js
@@ -1,13 +1,24 @@
+import {useEffect} from 'react';
 import PageContainer from './page-container';
 import Header from './header';
 import Footer from './footer';
 
 export default function Page({ title, description, children }) {
+  useEffect(() => {
+    if(window){
+      const SmoothScroll = require('smooth-scroll');
+      new SmoothScroll('a[href*="#"]', {
+        speed: 1000,
+        speedAsDuration: true,
+      })
+    }
+  }, []);
+
   return (
     <PageContainer title={title} description={description}>
       <Header />
 
-      <div className="content">{children}</div>
+      <div className="content" id="content">{children}</div>
 
       <Footer />
       <style jsx>{`
@@ -15,8 +26,14 @@ export default function Page({ title, description, children }) {
           display: flex;
           align-items: center;
           flex-direction: column;
-          width: 95%;
-          max-width: 1700px;
+          width: 100%;
+          background: azure;
+          padding-top: 50px;
+        }
+        @media (max-width: 1256px) {
+          .content {
+            padding-top: 0;
+          }
         }
       `}</style>
     </PageContainer>
